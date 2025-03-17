8c9d4700ebec75c1d85e86501448c2e11c892e08
sarkars <sarkars@strad.nygenome.org>
2025-03-16 20:19:14 -0400

    cleaned __init__ to get rid of old structure made by cursor

--- src/panhumanpy/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-panhumanpy - A Python package for automated cell type annotation using neural networks
-"""
-from . import core, postprocessing, preprocessing, utils
-
-__version__ = "0.1.0"
-
-
-__all__ = ["core", "preprocessing", "postprocessing", "utils"]
