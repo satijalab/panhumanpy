9fe15ae6841ec8ecebfbe9a28d344b4117bd1be7
sarkars <souravsarkar@nygenome.org>
2025-03-19 11:44:07 -0400

    model meta dict replaced pkl file with py module

---
+++ src/panhumanpy/tools/inference_model/model_meta.py
@@ -0,0 +1,10 @@
+##########################
+## model metadata ########
+
+model_meta = {
+    'inference_model_name': 'M0.2',
+    'inference_model_loss_function': 'level_weighted_focal_loss',
+    'max_depth': 8,
+    'inference_model_embedding_layer': 'dense_3',
+    'feature_panel_size': 5039
+}
