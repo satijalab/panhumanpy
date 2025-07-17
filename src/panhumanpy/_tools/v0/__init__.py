4d07d5a541ccb292609bf53354763de4705d7ffe
sarkars <sarkars@strad.nygenome.org>
2025-07-17 12:40:15 -0400

    Modified init files for new tools dir structure.

--- src/panhumanpy/_tools/v0/__init__.py
+++ src/panhumanpy/_tools/v0/__init__.py
@@ -1,3 +1,12 @@
 """
-Internal resources and tools for the panhumanpy package.
+keras model  metadata for internal use by the package for model version
+v0.
 """
+
+model_meta = {
+    'inference_model_name': 'M0.2',
+    'inference_model_loss_function': 'level_weighted_focal_loss',
+    'max_depth': 8,
+    'inference_model_embedding_layer': 'dense_3',
+    'feature_panel_size': 5039
+}
