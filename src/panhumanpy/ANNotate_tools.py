536863e03c75110517079a92dfde3cdc01039301
sarkars <sarkars@strad.nygenome.org>
2025-07-17 17:45:08 -0400

    Added calibration rel. changes to load_model_meta

--- src/panhumanpy/ANNotate_tools.py
+++ src/panhumanpy/ANNotate_tools.py
@@ -1403,7 +1403,33 @@ class InferenceTools():
             assert isinstance(meta_dict['feature_panel_size'], int),(
                 "feature_panel_size must be an integer."
             )
-        # enforce existence of certain key:vals here
+        
+        assert 'calibration' in meta_dict.keys(), (
+            "model metadata dict must have a key 'calibration'."
+        )
+        
+        assert 'calibrators' in meta_dict.keys(), (
+            "model metadata dict must have a key 'calibrators'."
+        )
+
+        if meta_dict['calibration'] is not None:
+            assert isinstance(meta_dict['calibration'], str), (
+                "calibration must be a string or None."
+            )
+        
+        assert isinstance(meta_dict['calibrators'], list), (
+            "calibrators must be a list."
+        )
+
+        if meta_dict['calibration'] is None:
+            assert len(meta_dict['calibrators'])==0, (
+                "'calibration' and 'calibrators' entries mutually inconsistent." 
+            )
+        else:
+            assert len(meta_dict['calibrators'])>0, (
+                "calibrator model names should be provided when calibration \
+                not None."
+            )
 
         return meta_dict
 
