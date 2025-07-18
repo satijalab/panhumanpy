5c39c961030c58af2fece2cc22d32aaf10bd68b8
sarkars <sarkars@strad.nygenome.org>
2025-07-18 12:09:19 -0400

    Added dir for model vers v1 and init files

---
+++ src/panhumanpy/_tools/v1/__init__.py
@@ -0,0 +1,14 @@
+"""
+keras model  metadata for internal use by the package for model version
+v1.
+"""
+
+model_meta = {
+    'inference_model_name': 'M0.2',
+    'inference_model_loss_function': 'level_wt_focal_loss',
+    'max_depth': 8,
+    'inference_model_embedding_layer': 'dense_3',
+    'feature_panel_size': 5055,
+    'calibration': None,
+    'calibrators':[]
+}
